45c473ccb352564988cd72401229fd763ddecfcd
Adam Sobotka <vorcigernix@gmail.com>
2023-09-04 08:13:16 +0200

    delete unnecessary files

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,8 +7,7 @@ import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/greet/[name].tsx";
 import * as $4 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
-import * as $$1 from "./islands/Mint.tsx";
+import * as $$0 from "./islands/Mint.tsx";
 
 const manifest = {
   routes: {
@@ -19,8 +18,7 @@ const manifest = {
     "./routes/index.tsx": $4,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
-    "./islands/Mint.tsx": $$1,
+    "./islands/Mint.tsx": $$0,
   },
   baseUrl: import.meta.url,
 };
